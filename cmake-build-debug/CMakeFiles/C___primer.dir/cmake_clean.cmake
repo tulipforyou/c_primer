2b425bb75eaaa4e1c1346f7ff03e5cfe7f4e7471
tulipforyou <tulipforyou@mail.hfut.edu.cn>
2019-03-30 20:06:44 +0800

    version 0.2

---
+++ cmake-build-debug/CMakeFiles/C___primer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___primer.dir/main.cpp.o"
+  "CMakeFiles/C___primer.dir/firstProgram.cpp.o"
+  "CMakeFiles/C___primer.dir/secondProgram.cpp.o"
+  "CMakeFiles/C___primer.dir/thirdProgram.cpp.o"
+  "C___primer.pdb"
+  "C___primer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___primer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
